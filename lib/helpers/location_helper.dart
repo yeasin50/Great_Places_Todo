61c4edd6da49703d4ebb8e7efabe8f5e6f4368df
MD.Yeasin Sheikh <46500228+yeasin50@users.noreply.github.com>
2020-10-17 22:15:28 +0600

    instructions

--- lib/helpers/location_helper.dart
+++ lib/helpers/location_helper.dart
@@ -1,7 +1,7 @@
 import 'dart:convert';
 
 import 'package:http/http.dart' as http;
-
+//TODO:: Add your Api KEY
 const GOOGLE_API_KEY = ' ';
 
 class LocationHelper {
--- lib/screens/map_screen.dart
+++ lib/screens/map_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
+import "package:google_maps_flutter/google_maps_flutter.dart";
 class MapScreen extends StatefulWidget {
   MapScreen({Key key}) : super(key: key);
 
